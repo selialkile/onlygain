7e3775a3072b38c8b07c2522799f07618a5379a5
Thiago Coutinho <thiago.coutinho@locaweb.com.br>
2015-09-21 15:07:42 -0300

    update

--- grafico.js
+++ grafico.js
@@ -5,6 +5,7 @@
  * 1. Run to evaluate the selected text (Cmd-R),
  * 2. Inspect to bring up an Object Inspector on the result (Cmd-I), or,
  * 3. Display to insert the result in a comment after the selection. (Cmd-L)
+ http://www.flotcharts.org/flot/examples/resize/index.html
  */
 
 
@@ -20,29 +21,26 @@ s = document.createElement('script');
 s.src = 'https://raw.githubusercontent.com/selialkile/onlygain/master/jquery.flot.resize.js';
 document.body.appendChild(s);
 
-$('body').prepend('<div id="graficopah" style="position:absolute; top:1px; left1px; width: 500px; height: 300px; z-index: 2000; background-color: red;" class=".demo-container"><div style="padding: 0px; position: relative;" id="placeholder"></div></div>');
+$('body').prepend('<div id="graficopah" style="position:absolute; top:300px; left1px; width: 500px; height: 300px; z-index: 2000; background-color: white;" class=".demo-container"><div style="width: 500px; height: 300px; position: relative;" id="placeholder"></div></div>');
 
 $(function() {
 
-		var d5 = [];
-		for (var i = 0; i < 14; i += 0.5) {
-			d5.push([i, i-1]);
-		}
+	var d1 = [];
+	for (var i = 0; i < 14; i += 0.5) {
+		d1.push([i, Math.sin(i)]);
+	}
 
-    $("#graficopah").width(350);
-    $("#graficopah").height(250);
+	var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+	var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
 
-		$.plot("#graficopah #placeholder", [{
-			data: d5,
-			lines: { show: true },
-			points: { show: true }
-		}]);
+	var placeholder = $("#graficopah #placeholder");
+	var plot = $.plot(placeholder, [d1, d2, d3]);
 
-  	$(".demo-container").resizable({
-			maxWidth: 350,
-			maxHeight: 250,
-			minWidth: 150,
-			minHeight: 100
-		});
-
+	$("#graficopah").resizable({
+		maxWidth: 350,
+		maxHeight: 200,
+		minWidth: 150,
+		minHeight: 50
 	});
+
+});
